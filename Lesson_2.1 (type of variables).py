4dc36e40f7b76e0d429bc1d402726635a81ceb24
Никита <nicky.n8@yandex.ru>
2020-04-02 23:10:04 +0300

    Test commit

---
+++ Lesson_2.1 (type of variables).py
@@ -0,0 +1,32 @@
+# Типы переменных (на примере автомобилей)
+
+# Марка
+
+name = "Ford"
+
+print(type(name))
+
+# Возраст
+
+age = 3
+
+print(type(age))
+
+# Объем двигателя
+
+engine_volume = 1.6
+
+print(type(engine_volume))
+
+# Есть ли люк
+
+see_sky = False
+
+print(type(see_sky))
+
+# Вывод значений переменных вместе с типом
+
+print(name, type(name))
+print(age, type(age))
+print(engine_volume, type(engine_volume))
+print(see_sky, type(see_sky))
